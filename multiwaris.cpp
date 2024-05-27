b740bce78778a91ad68dcef22a59c55fc0742eb1
halimbagusalhakim <halsat132@gmail.com>
2024-05-27 15:14:07 +0700

    multiwaris selesai

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -2,5 +2,47 @@
 using namespace std;
 
 class orang {
-    
+public:
+    int umur;
+
+    orang(int pUmur):
+        umur(pUmur){
+            cout << "Orang dibuat dengan umur " << umur << endl;
+        }
 };
+
+class pekerja : virtual public orang {
+public:
+
+    pekerja(int pUmur):
+        orang(pUmur) {
+            cout << "Pekerja dibuat\n" << endl;
+        }
+};
+
+class pelajar : virtual public orang {
+public:
+
+    pelajar(int pUmur) :
+        orang(pUmur) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+
+};
+
+class budi : public pekerja, public pelajar {
+public:
+    
+    budi(int pUmur) :
+        pekerja(pUmur), pelajar(pUmur), orang(pUmur)//hal ini dilakukan jika menggunakan virtual
+        {
+            cout << "Budi dibuat\n" << endl;
+
+        }
+};
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
